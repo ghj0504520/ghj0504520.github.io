6ab5376f7984dc5ae5b670b8d876b0295bf46b31
ghj0504520 <ghj0504520.cywjjy@gmail.com>
2016-09-03 00:32:38 +0800

    2016/09/03

--- ui.js
+++ ui.js
@@ -14,12 +14,13 @@ function heightScroll(){
     $(window).scroll(function(){
 
         var nowTop = $(this).scrollTop();
-        $('#scrollCount').remove();
-        $('<p id="scrollCount">' + nowTop + '</p>').appendTo($('#scrollTop'));
-
         var zoom=(window.outerWidth / window.innerWidth)*100;
 
-        if(zoom>100 && nowTop>=430)
+        $('#scrollCount').remove();
+
+        $('<p id="scrollCount">' + nowTop + '</p>').appendTo($('#scrollTop'));
+
+        if(zoom>=100 && nowTop>=430)
         {
             $('.footer').fadeIn();
         }
